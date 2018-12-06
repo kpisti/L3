38b3baba2c5df3fe70f1fc5a6f16c9ccc829c3ee
kpisti <39310152+kpisti@users.noreply.github.com>
2018-12-06 18:46:06 -0500

    Add files via upload

---
+++ L3.cpp
@@ -0,0 +1,164 @@
+/***********************************************************/
+/* L3 Link Prediction                                      */
+/*                                                         */
+/* Istvan Kovacs                                           */
+/***********************************************************/
+#include <math.h>
+#include <algorithm>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fstream>
+#include <set>
+#include <iostream>
+#include <time.h>
+#include "binheap_2.h"
+#include <map>
+#include <iomanip>
+
+using namespace std;
+typedef std::map<int, double> doublemap;
+typedef std::map<int, doublemap> doublematrix;
+doublematrix d;
+doublemap deg;
+long long maxid;
+typedef BinaryHeap<double, long long, std::greater_equal<double> >    min_heap;
+min_heap                   m_h;
+min_heap::pair_type        m_he;
+
+int main(int argc, char** argv)
+	{//ido=(int) time(NULL);
+	double s;
+	int e1,e2;
+    int ind;
+    char *fg;fg=argv[1];FILE *lg;
+    if(!(lg=fopen(fg,"r"))) {fprintf(stderr,"Bemeneti h fajl megnyitasa sikertelen!\n");exit(-1);}
+    d.clear();
+    
+    while (!feof(lg))
+        {fscanf(lg,"%d",&e1);
+        fscanf(lg,"%d",&e2);
+        fscanf(lg,"%lf",&s);
+        if (s!=0)
+            {ind=0;
+            if (d.find(e1)!=d.end())
+                {if (d[e1].find(e2)!=d[e1].end())
+                    {ind=1;
+                    }
+                }
+            if (ind==0)
+                {if (e1!=e2)
+                    {d[e1][e2]=s;
+                    d[e2][e1]=s;
+                    }
+                else
+                    {d[e1][e1]=s;
+                    }
+                }
+            }
+        fscanf(lg,"\n");
+        }
+    fclose(lg);
+
+    maxid=0;
+    for(doublematrix::iterator ii=d.begin(); ii!=d.end(); ii++)
+        {deg[ii->first]=0;
+        if (maxid<ii->first)
+            {maxid=ii->first;
+            }
+        }
+    maxid++;
+        
+    for(doublematrix::iterator ii=d.begin(); ii!=d.end(); ii++)
+        {for(doublemap::iterator ij=ii->second.begin(); ij!=ii->second.end();ij++)
+            {if (ii->first<ij->first)
+                {deg[ii->first]+=fabs(ij->second);
+                deg[ij->first]+=fabs(ij->second);
+                }
+            else
+                {deg[ii->first]+=fabs(ij->second);
+                }
+            }
+        }
+        
+    char lag[255];
+    sprintf(lag,"L3_predictions_%s.dat",fg);
+    ofstream l(lag, ios::out);
+    if (!l)
+        {cout << "Error during file operation!\n";
+        exit(-1);
+        };
+    
+    doublematrix h2;
+    for(doublematrix::iterator i=d.begin(); i!=d.end();i++ )
+        {for(doublemap::iterator hi=i->second.begin(); hi!=i->second.end(); hi++)
+            {for(doublemap::iterator hj=d[hi->first].begin(); hj!=d[hi->first].end(); hj++)
+                {if (hj->first<=i->first)
+                    {if (h2.find(i->first)!=h2.end())
+                        {if (h2[i->first].find(hj->first)!=h2[i->first].end())
+                            {h2[i->first][hj->first]+=1.0/sqrt((double) deg[hi->first]);
+                            if(hj->first!=i->first)
+                                {h2[hj->first][i->first]+=1.0/sqrt((double) deg[hi->first]);
+                                }
+                            }
+                        else
+                            {h2[i->first][hj->first]=1.0/sqrt((double) deg[hi->first]);
+                            h2[hj->first][i->first]=1.0/sqrt((double) deg[hi->first]);
+                            }
+                        }
+                    else
+                        {h2[i->first][hj->first]=1.0/sqrt((double) deg[hi->first]);
+                        h2[hj->first][i->first]=1.0/sqrt((double) deg[hi->first]);
+                        }
+                    }
+                }
+            }
+        }
+        
+    doublematrix h3;
+    for(doublematrix::iterator i=d.begin(); i!=d.end();i++ )
+        {for(doublemap::iterator j=i->second.begin(); j!=i->second.end(); j++)
+            {for(doublemap::iterator hi=h2[i->first].begin(); hi!=h2[i->first].end();hi++ )
+                {if (hi->first<=j->first)
+                    {if (h3.find(hi->first)!=h3.end())
+                        {if (h3[hi->first].find(j->first)!=h3[hi->first].end())
+                            {h3[hi->first][j->first]+=hi->second/sqrt((double) deg[i->first]);
+                            if(hi->first!=j->first)
+                                {h3[j->first][hi->first]+=hi->second/sqrt((double) deg[i->first]);
+                                }
+                            }
+                        else
+                            {h3[hi->first][j->first]=hi->second/sqrt((double) deg[i->first]);
+                            h3[j->first][hi->first]=hi->second/sqrt((double) deg[i->first]);
+                            }
+                        }
+                    else
+                        {h3[hi->first][j->first]=hi->second/sqrt((double) deg[i->first]);
+                        h3[j->first][hi->first]=hi->second/sqrt((double) deg[i->first]);
+                        }
+                    }
+                }
+            }
+        }
+        
+    m_h.clear();
+    for(doublematrix::iterator ii=h3.begin(); ii!=h3.end();ii++ )
+        {for(doublemap::iterator hi=ii->second.begin(); hi!=ii->second.end(); hi++)
+            {if (hi->first<ii->first)
+                {if (d[ii->first].find(hi->first)==d[ii->first].end())
+                    {m_h.push(hi->second, maxid*((long long) (ii->first))+((long long) (hi->first)));
+                    }
+                }
+            }
+        }
+        
+    int xi, xj;
+    while ((!m_h.isEmpty()))
+        {m_he = m_h.pop();
+        xi=m_he.second/maxid;
+        xj=m_he.second-((long long) xi)*maxid;
+        if (xi!=xj)
+            {l << xi << "\11"<<xj << "\11" << m_he.first<< "\n";
+            }
+        }
+	return 0;
+	}
---
+++ binheap_2.h
@@ -0,0 +1,226 @@
+#ifndef BINARY_HEAP_HPP
+#define BINARY_HEAP_HPP
+
+#include <vector>
+#include <queue>
+
+template <class Heap>
+struct binary_heap_traits {
+   typedef typename Heap::key_type                    key_type;
+   typedef typename Heap::element_type                element_type;
+   typedef typename Heap::pair_type                   pair_type;
+};
+
+template <class Key, class Element, class Cmp = std::less<Key> >
+class BinaryHeap {
+public:
+   typedef Key                                        key_type;
+   typedef Element                                    element_type;
+   typedef std::pair<Key, Element>                    pair_type;
+
+   BinaryHeap();
+   bool isEmpty() const;
+   bool hasElement(int id) const;
+   int push(const Key &k, const Element &e);
+   pair_type pop();
+   pair_type pop(int id);
+   pair_type read(int id);
+   const pair_type& top();
+   void move(int id, const Key &new_key);
+	void clear();
+private:
+   typedef std::vector<pair_type>                     evector_type;
+   typedef std::vector<int>                           idmap_type;
+   typedef std::queue<int>                            idq_type;
+
+   Cmp                                                cmp;
+   evector_type                                       v;
+   idmap_type                                         id2vpos, vpos2id;
+   idq_type                                           idq;
+
+   void heapUp(int index);
+   void heapDown(int index);
+   void heap(int index);
+};
+
+template <class Key, class Element, class Cmp>
+BinaryHeap<Key, Element, Cmp>::BinaryHeap() {
+}
+
+template <class Key, class Element, class Cmp>
+bool BinaryHeap<Key, Element, Cmp>::isEmpty() const {
+   return v.empty();
+}
+		
+template <class Key, class Element, class Cmp>
+void BinaryHeap<Key, Element, Cmp>::clear() {
+   v.clear();
+   id2vpos.clear();
+   vpos2id.clear();
+   idq=idq_type();
+}
+
+template <class Key, class Element, class Cmp>
+bool BinaryHeap<Key, Element, Cmp>::hasElement(int id) const {
+   return (id < (int)id2vpos.size() && id2vpos[id] != -1);
+}
+
+template <class Key, class Element, class Cmp>
+int BinaryHeap<Key, Element, Cmp>::push(const Key &k, const Element &e) {
+   int         v_pos, id;
+
+   v.push_back(pair_type(k, e));
+   v_pos = v.size() - 1;
+   if (idq.empty()) {
+      id2vpos.push_back(v_pos);
+      id = id2vpos.size() - 1;
+      vpos2id.push_back(id);
+   } else {
+      id = idq.front();
+      idq.pop();
+      id2vpos[id] = v_pos;
+      vpos2id[v_pos] = id;
+   }
+
+   heapUp(v_pos);
+
+   return id;
+}
+
+template <class Key, class Element, class Cmp>
+typename BinaryHeap<Key, Element, Cmp>::pair_type BinaryHeap<Key, Element, Cmp>::pop() {
+   int               id, v_pos;
+   pair_type         max_e;
+
+   v_pos = v.size() - 1;
+
+   max_e = v[0];
+   v[0] = v[v_pos];
+   v.pop_back();
+
+   id = vpos2id[0];
+   idq.push(id);
+   id2vpos[id] = -1;
+
+   id = vpos2id[v_pos];
+   vpos2id[0] = id;
+   id2vpos[id] = 0;
+
+   heapDown(0);
+   
+   return max_e;
+}
+
+template <class Key, class Element, class Cmp>
+typename BinaryHeap<Key, Element, Cmp>::pair_type BinaryHeap<Key, Element, Cmp>::pop(int id) {
+   pair_type            max_e;
+   int                  v_pos, n = v.size();
+
+   v_pos = id2vpos[id];
+   max_e = v[v_pos];
+   idq.push(id);
+   id2vpos[id] = -1;
+
+   if (v_pos < n - 1) {
+      v[v_pos] = v[n - 1];
+      v.pop_back();
+      
+      id = vpos2id[n - 1];
+      vpos2id[v_pos] = id;
+      id2vpos[id] = v_pos;
+
+      heap(v_pos);
+   } else
+      v.pop_back();
+   
+   return max_e;
+}
+
+template <class Key, class Element, class Cmp>
+typename BinaryHeap<Key, Element, Cmp>::pair_type BinaryHeap<Key, Element, Cmp>::read(int id) {
+   return v[id2vpos[id]];
+}
+
+template <class Key, class Element, class Cmp>
+const typename BinaryHeap<Key, Element, Cmp>::pair_type& BinaryHeap<Key, Element, Cmp>::top() {
+   return v[0];
+}
+
+template <class Key, class Element, class Cmp>
+void BinaryHeap<Key, Element, Cmp>::move(int id, const Key &new_key) {
+   int                  v_pos = id2vpos[id];
+
+   if (cmp(new_key, v[v_pos].first)) {
+      v[v_pos].first = new_key;
+      heapUp(v_pos);
+   } else {
+      v[v_pos].first = new_key;
+      heapDown(v_pos);
+   }
+}
+
+template <class Key, class Element, class Cmp>
+void BinaryHeap<Key, Element, Cmp>::heapUp(int index) {
+   int            parent, t;
+   pair_type      temp;
+
+   parent = (index - 1) >> 1;
+   while (index > 0 && cmp(v[index].first, v[parent].first)) {
+      temp = v[index];
+      v[index] = v[parent];
+      v[parent] = temp;
+
+      t = vpos2id[index];
+      vpos2id[index] = vpos2id[parent];
+      vpos2id[parent] = t;
+      
+      t = id2vpos[vpos2id[index]];
+      id2vpos[vpos2id[index]] = id2vpos[vpos2id[parent]];
+      id2vpos[vpos2id[parent]] = t;
+
+      index = parent;
+      parent = (index - 1) >> 1;
+   }
+}
+
+template <class Key, class Element, class Cmp>
+void BinaryHeap<Key, Element, Cmp>::heapDown(int index) {
+   int            child, n, t;
+   pair_type      temp;
+
+   n = v.size();
+
+   child = (index << 1) + 1;
+   if (child < n - 1 && cmp(v[child + 1].first, v[child].first))
+      child++;
+   while (child < n && cmp(v[child].first, v[index].first)) {
+      temp = v[index];
+      v[index] = v[child];
+      v[child] = temp;
+      
+      t = vpos2id[index];
+      vpos2id[index] = vpos2id[child];
+      vpos2id[child] = t;
+      
+      t = id2vpos[vpos2id[index]];
+      id2vpos[vpos2id[index]] = id2vpos[vpos2id[child]];
+      id2vpos[vpos2id[child]] = t;
+
+      index = child;
+      child = (index << 1) + 1;
+      if (child < n - 1 && cmp(v[child + 1].first, v[child].first))
+         child++;
+   }
+}
+
+template <class Key, class Element, class Cmp>
+void BinaryHeap<Key, Element, Cmp>::heap(int index) {
+   if (index > 0 && cmp(v[index].first, v[(index - 1) >> 1].first))
+      heapUp(index);
+   else
+      heapDown(index);
+}
+
+
+#endif
+
